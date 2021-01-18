ad08266735fd2d8691714fa573dfd0dbade5c5f7
Ashish Yadav <ashudva@gmail.com>
2021-01-19 00:52:36 +0530

    Add new files

---
+++ HAR_DM/Practice Work/myfirst.R
@@ -0,0 +1,17 @@
+# assigning values to variables
+a <- 2
+b <- -1
+c <- -4
+
+# solving the quadratic equation
+x <-(-b + sqrt(b^2 - 4*a*c))/(2*a)
+y <-(-b - sqrt(b^2 - 4*a*c))/(2*a)
+print(x)
+print(y)
+
+# Basic operations 
+z = log(1024, base = 4)
+k = log(1024, 4)
+# Use ?function or help("function") to find help about the function
+# Use args(function) to get info about the args of the function
+# For operators don't use "" afer ?
